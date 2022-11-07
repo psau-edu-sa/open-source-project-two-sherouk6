724eb0b74de0ff090e9c0322be207f138c9327c4
Sherouk Mhmoud Mohamed <104649605+sherouk6@users.noreply.github.com>
2022-11-07 21:57:32 +0300

    first commit

---
+++ predict.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch import nn
+from torch import tensor
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+import torchvision.models as models
+from collections import OrderedDict
+import json
+import PIL
+from PIL import Image
+import argparse
+
+import futils
+
+#Command Line Arguments
+
+argumentparser = argparse.ArgumentParser(
+    description='predict-file')
+argumentparser.add_argument('input_img', default='mudigosa/flowers/test/1/image_06752.jpg', nargs='*', action="store", type = str)
+argumentparser.add_argument('checkpoint', default='/home/workspace/mudigosa/checkpoint.pth', nargs='*', action="store",type = str)
+argumentparser.add_argument('--top_k', default=5, dest="top_k", action="store", type=int)
+argumentparser.add_argument('--category_names', dest="category_names", action="store", default='mudigosa/mapping/cat_to_name.json')
+argumentparser.add_argument('--gpu', default="gpu", action="store", dest="gpu")
+
+parser = argumentparser.parse_args()
+path_images = parser.input_img
+number_of_outputs = parser.top_k
+power = parser.gpu
+input_img = parser.input_img
+path = parser.checkpoint
+category_names = parser.category_names
+
+
+
+training_loader, testing_loader, validation_loader = futils.load_data()
+
+
+futils.load_checkpoint(path)
+
+
+with open('category_names', 'r') as json_file:
+    cat_to_name = json.load(json_file)
+
+
+probability = futils.predict(path_images, models, number_of_outputs, power)
+
+
+labels = [cat_to_name[str(index + 1)] for index in np.array(probability[1][0])]
+probabilities = np.array(probability[0][0])
+
+
+i=0
+while i < number_of_outputs:
+    print("{} with a probability of {}".format(labels[i], probabilities[i]))
+    i += 1
+
+print("Here you are")
---
+++ train.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import torch
+from torch import nn
+from torch import tensor
+from torch import optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torchvision import datasets, transforms
+import torchvision.models as models
+import argparse
+
+import futils
+
+argumentparser = argparse.ArgumentParser(description='train.py')
+# Command Line ardguments
+
+argumentparser.add_argument('data_dir', nargs='*', action="store", default="./flowers/")
+argumentparser.add_argument('--gpu', dest="gpu", action="store", default="gpu")
+argumentparser.add_argument('--save_dir', dest="save_dir", action="store", default="./checkpoint.pth")
+argumentparser.add_argument('--learning_rate', dest="learning_rate", action="store", default=0.001)
+argumentparser.add_argument('--dropout', dest = "dropout", action = "store", default = 0.5)
+argumentparser.add_argument('--epochs', dest="epochs", action="store", type=int, default=1)
+argumentparser.add_argument('--arch', dest="arch", action="store", default="vgg16", type = str)
+argumentparser.add_argument('--hidden_units', type=int, dest="hidden_units", action="store", default=120)
+
+parser = argumentparser.parse_args()
+where = parser.data_dir
+paths = parser.save_dir
+lr = parser.learning_rate
+structures = parser.arch
+dropout = parser.dropout
+hidden_layer = parser.hidden_units
+power = parser.gpu
+epochs = parser.epochs
+
+
+trainloader, v_loader, testloader = futils.load_data(where)
+
+
+model, optimizer, criterion = futils.setup(structures,dropout,hidden_layer,lr,power)
+
+
+futils.training_network(model, optimizer, criterion, epochs, 20, trainloader, power)
+
+
+futils.save_checkpoint(paths,structures,hidden_layer,dropout,lr)
+
+
+print("All Set and Done. The Model is trained") # Coffee timeee
